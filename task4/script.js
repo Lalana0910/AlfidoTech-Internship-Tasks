bbd1fe975a766af34492951abb3714f7e223d6f3
Lalana0910 <lalanaa.jai@gmail.com>
2025-11-18 00:31:24 +0530

    Update script.js

--- task4/script.js
+++ task4/script.js
@@ -1,1 +1,27 @@
 <!-- Task 4 code to be added -- >
+  document.getElementById("addBtn").addEventListener("click", addTask);
+
+function addTask() {
+    const taskText = document.getElementById("taskInput").value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("deleteBtn");
+
+    deleteBtn.onclick = function() {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+
+    document.getElementById("taskList").appendChild(li);
+    document.getElementById("taskInput").value = "";
+}
