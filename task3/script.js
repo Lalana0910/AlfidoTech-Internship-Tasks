f45e64951f537444eecdee9a723634b2c8c78dff
Lalana0910 <lalanaa.jai@gmail.com>
2025-11-18 00:28:02 +0530

    Update script.js

--- task3/script.js
+++ task3/script.js
@@ -1,1 +1,13 @@
 <!-- Task 3 code to be added -- >
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+document.getElementById("colorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = getRandomColor();
+});
